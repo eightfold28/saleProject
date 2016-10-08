e9337a7fe0ee79a95b3ec288a1b0a96a1bfc04b8
Raka Nurul Fikri <raka_rukri@yahoo.com>
2016-10-08 22:32:51 +0700

    fix navigation | unfinished : AJAX

--- catalog.php
+++ catalog.php
@@ -21,8 +21,8 @@
 	<div class="navigation_bar">
 		<ul>
 			<li><a class="active" href= <?php echo 'catalog.php?active_ID=' . $activeid?> >Catalog</a></li>
-			<li><a href="">Your Products</a></li>
-			<li><a href="">Add Product</a></li>
+			<li><a href= <?php echo 'yourproduct.html?active_ID=' . $activeid ?> >Your Products</a></li>
+			<li><a href= <?php echo 'addproduct.html?active_ID=' . $activeid ?> >Add Product</a></li>
 			<li><a href= <?php echo 'sales.php?active_ID=' . $activeid?> >Sales</a></li>
 			<li><a href= <?php echo 'purchases.php?active_ID=' . $activeid ?> >Purchases</a></li>
 		</ul>
