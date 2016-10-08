319ab0456817040dc7b5db8c7fe78f2931d3d978
eightfold28 <13514028@std.stei.itb.ac.id>
2016-10-08 14:22:12 +0700

    include ini buat greet user

---
+++ userlogin.php
@@ -0,0 +1,27 @@
+<link rel="stylesheet" href="sales.css" type="text/css">
+
+<?php
+	// session_start();
+	$servername = "localhost";
+	$dbusername = "root";
+	$dbpassword = "";
+	$dbname = "tubeswbd1";
+
+	$con = new mysqli($servername, $dbusername, $dbpassword, $dbname);
+	
+	if ($con->connect_error) {
+		die("connection failed: " . $con->connect_error);
+	}
+	$activeid = $_GET['active_ID'];
+
+	$sql = "SELECT Username FROM user WHERE active_ID = '$activeid' ";
+	$result = mysqli_query($con, $sql);
+	while ( $row = mysqli_fetch_array($result)) { ?>
+		<?php $username = $row['Username']; ?>
+		<div class= "user_greet">
+        <br>
+        Hi, <?php echo $row['Username']; ?>! <br>
+        <a href= "logout.php">logout</a>
+        <br>
+   		</div>
+	<?php }?>
